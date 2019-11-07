2b00c80f0960a5857e30f359727fa3926c3f106f
nacimbob <fn_boubrit@esi.dz>
2019-11-07 18:02:01 +0100

    This is my first commit!

---
+++ src/test/java/com/cve/cve/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cve.cve;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
